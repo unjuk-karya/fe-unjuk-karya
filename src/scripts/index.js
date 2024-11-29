991b9ae3c6d3821a944ed1b17300a23dcaa9ffa4
Muhamamd Dila <muhammaddila.all@gmail.com>
2024-11-29 15:07:34 +0700

    Creating Register Page

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,6 +1,7 @@
 import 'regenerator-runtime';
 import '../styles/style.css';
 import '../styles/responsive.css';
+import './views/components/index-component.js';
 import App from './views/app';
 
 const app = new App({
